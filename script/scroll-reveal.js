729859dbe367e30281afe3adc5a8ac404d5fcb2a
Victor Yan Marin de Souza <victor.souza@ska.com.br>
2025-06-29 15:50:24 -0300

    Fully completed (Desktop ONLY)

--- script/scroll-reveal.js
+++ script/scroll-reveal.js
@@ -1,16 +1,36 @@
- const reveals = document.querySelectorAll(".reveal");
+  function handleReveal() {
+    const reveals = document.querySelectorAll('.reveal');
 
-  function revealOnScroll() {
-    for (let i = 0; i < reveals.length; i++) {
+    reveals.forEach(el => {
       const windowHeight = window.innerHeight;
-      const revealTop = reveals[i].getBoundingClientRect().top;
+      const elementTop = el.getBoundingClientRect().top;
+      const elementBottom = el.getBoundingClientRect().bottom;
       const revealPoint = 100;
 
-      if (revealTop < windowHeight - revealPoint) {
-        reveals[i].classList.add("active");
+      if (elementTop < windowHeight - revealPoint && elementBottom > 0) {
+        el.classList.add('active');
+      } else {
+        el.classList.remove('active');
       }
-    }
+    });
   }
 
-  window.addEventListener("scroll", revealOnScroll);
-  window.addEventListener("load", revealOnScroll);
+  window.addEventListener("scroll", handleReveal)
+   window.addEventListener("load", handleReveal)
+
+function revealPhoto() {
+    const elements = document.querySelectorAll(".photo-slide");
+
+    elements.forEach(el => {
+      const windowHeight = window.innerHeight;
+      const elementTop = el.getBoundingClientRect().top;
+      const visibilityThreshold = 150;
+
+      if (elementTop < windowHeight - visibilityThreshold) {
+        el.classList.add("active");
+      }
+    });
+  }
+
+  window.addEventListener("scroll", revealPhoto);
+  window.addEventListener("load", revealPhoto);
